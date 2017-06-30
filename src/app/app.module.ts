0d4470e838d2392bdb702da43424803dc4f5973c
sandeep <sandeepgoud0612@gmail.com>
2017-06-29 22:18:54 -0400

    UI Updates.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    PaymentComponent    
   ],
   providers: [],
   bootstrap: [AppComponent]
