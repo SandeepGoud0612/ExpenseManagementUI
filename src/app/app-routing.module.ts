26cf31cf212e6eb8aff6082cd960f31ea7f6e074
sandeep <sandeepgoud0612@gmail.com>
2017-06-30 23:00:15 -0400

    UI Updates.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PaymentComponent } from './payment/payment.component';
+import { ProjectComponent } from './project/project.component';
 
 const routes: Routes = [
     {
         path: '',
-        component: PaymentComponent
+        component: ProjectComponent
     }
 ];
 
